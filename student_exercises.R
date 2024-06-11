da8479d79e4d83e74df73a42c677d2174981638f
jminimi <63049938+jminimi@users.noreply.github.com>
2024-06-11 18:35:27 +0100

    Add files via upload

---
+++ student_exercises.R
@@ -0,0 +1,8 @@
+## ---------------------------
+## This is an .R file for you to code along during the workshop
+## The material for the workshop is available in workshop_material.html
+## Any questions during the session, please ask one of the instructors
+## ---------------------------
+
+# This is a comment
+print("Hello world!")
